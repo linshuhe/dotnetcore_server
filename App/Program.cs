63f90017c03daf7cda2ad9d9d95cf9909e713c89
linshuhe1 <695671725@qq.com>
2019-09-25 14:40:38 +0800

    first commit

---
+++ App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Model;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello App!");
+
+            Console.WriteLine(Model.Test.Msg);
+        }
+    }
+}
---
+++ Model/Test.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Model
+{
+    public class Test
+    {
+        public static String Msg = "Test Class lib: 0.0.3";
+    }
+}
